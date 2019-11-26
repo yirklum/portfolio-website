cfbfdd6bc145fa05980df928fdfcc9e9e18b7cf6
Yirklum <yirklum@aol.com>
2019-11-26 01:11:09 -0500

    ready

--- public/javascript/contact.js
+++ public/javascript/contact.js
@@ -1,34 +1,39 @@
-
 $(document).ready(function() {
-    $("body").removeClass("fade-out");
+    // $("body").removeClass("fade-out");
 });
 
 function validateForm() {
-    var x = document.forms["contact"]["first-name"].value;
-    var y = document.forms["contact"]["last-name"].value;
-    var z = document.forms["contact"]["email"].value;
-    var a = document.forms["contact"]["description"].value;
+    // var x = document.forms["contact"]["first-name"].value;
+    // var y = document.forms["contact"]["last-name"].value;
+    // var z = document.forms["contact"]["eMail"].value;
+    // var a = document.forms["contact"]["description"].value;
     
-    if (x == "") {
-        alert("First Name is required");
-        return false;
-    }
-    else if (y == "") {
-        alert("Last Name is required");
-        return false;
-    }
+    // if (x == "") {
+    //     alert("First Name is required");
+    //     return false;
+    // }
+    // else if (y == "") {
+    //     alert("Last Name is required");
+    //     return false;
+    // }
 
-    else if (z == "") {
-        alert("E-mail address is required");
-        return false;
-    }
+    // else if (z == "") {
+    //     alert("E-mail address is required");
+    //     return false;
+    // }
 
-    else if (a == "") {
-        alert("Description of request is required");
-        return false;
-    }
-    else {
+    // else if (a == "") {
+    //     alert("Description of request is required");
+    //     return false;
+    // }
+    // else {
         $("#confirmation").css("display", "inline-block");
         $(".contact-form").css("display", "none");
     }
-}
+// }
+
+
+
+
+
+
